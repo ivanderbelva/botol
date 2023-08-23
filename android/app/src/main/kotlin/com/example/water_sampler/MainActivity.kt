20f0ba751080c10cd40049a87782b3e16836b4e6
ivanderbelva <ivanderbelva@student.telkomuniversity.ac.id>
2023-08-23 18:27:12 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/water_sampler/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.water_sampler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import flutter_speech
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FlutterSpeechRecognitionPlugin.register(with: registry.registrar(forPlugin: "FlutterSpeechRecognitionPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
